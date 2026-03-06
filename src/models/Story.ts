f8d0eedc74308ef7f00466cbc5a5c72bd12ed0d3
Piotrek <piotrek@Piotreks-MacBook-Air.local>
2026-03-06 10:02:54 +0100

    story.ts

---
+++ src/models/Story.ts
@@ -0,0 +1,14 @@
+export type StoryPriority = "low" | "medium" | "high";
+
+export type StoryStatus = "todo" | "doing" | "done";
+
+export interface Story {
+  id: string;
+  name: string;
+  description: string;
+  priority: StoryPriority;
+  projectId: string;
+  createdAt: string;
+  status: StoryStatus;
+  ownerId: string;
+}
